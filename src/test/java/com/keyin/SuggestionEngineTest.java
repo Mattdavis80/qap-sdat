26bfdc27f867dd4bf3edd96d5714d7a9cad45ab4
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2024-01-28 23:21:11 -0330

    failed test added.

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -1,4 +1,12 @@
 package com.keyin;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SuggestionEngineTest {
+
+    @Test
+    public void testSuggestionEngine() {
+        Assertions.assertTrue(false);
+    }
 }
