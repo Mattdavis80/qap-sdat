1168a403aa16c34bb41f79a8b116d1a1e234a9f0
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2024-01-29 21:25:17 -0330

    Added 3 Unit Tests to project

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -2,11 +2,59 @@ package com.keyin;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class SuggestionEngineTest {
 
-//    @Test
-//    public void testSuggestionEngine() {
-//        Assertions.assertTrue(false);
-//    }
-}
+
+
+    // Test 1 - Ensure words.txt exists.
+    @Test
+    public void testWordsFileExists() {
+        Path wordsFile = Paths.get("src/main/resources/words.txt");
+        Assertions.assertTrue(Files.exists(wordsFile));
+    }
+
+    // Test 2 - Ensure setSuggestionsDatabase() setter works as intended.
+    @Test
+    public void testSetSuggestionsDatabase() {
+        SuggestionEngine testSuggestionEngine = new SuggestionEngine();
+
+        SuggestionsDatabase testSuggestionsDatabase = new SuggestionsDatabase();
+        testSuggestionsDatabase.getWordMap().put("test", 1);
+        testSuggestionEngine.setWordSuggestionDB(testSuggestionsDatabase);
+        Map<String, Integer> testMap = testSuggestionEngine.getWordSuggestionDB();
+
+        Map<String, Integer> comparisonMap = new HashMap<>();
+        comparisonMap.put("test", 1);
+
+        assertEquals(comparisonMap, testMap);
+    }
+
+    // Test 3 - Ensure GenerateSuggestions() works as intended.
+    @Test
+    public void testGenerateSuggestions() {
+        SuggestionEngine testSuggestionEngine = new SuggestionEngine();
+
+        SuggestionsDatabase testSuggestionsDatabase = new SuggestionsDatabase();
+        testSuggestionsDatabase.getWordMap().put("taste", 1);
+        testSuggestionEngine.setWordSuggestionDB(testSuggestionsDatabase);
+
+        String testString = testSuggestionEngine.generateSuggestions("taste");
+
+        assertEquals("", testString);
+
+    }}
+
+
+
+
+
+
+
